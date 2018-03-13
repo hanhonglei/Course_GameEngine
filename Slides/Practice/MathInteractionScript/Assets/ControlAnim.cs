c50db90d9ddfd3810243fadda2affe1386531c53
hanhonglei <hanhonglei@cuc.edu.cn>
2018-03-13 14:42:47 +0800

    no message

---
+++ Slides/Practice/MathInteractionScript/Assets/ControlAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAnim : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        Debug.Assert(anim != null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(anim != null)
+                anim.SetBool("isClick", true);
+        }	
+	}
+}
