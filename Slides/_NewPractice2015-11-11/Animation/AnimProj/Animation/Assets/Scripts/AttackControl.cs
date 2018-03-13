e00c5a5d2e424b6a0b3eed7093f9603f2c0a64cb
hanhonglei <hanhonglei@cuc.edu.cn>
2018-03-13 17:30:12 +0800

    New animation samples.

---
+++ Slides/_NewPractice2015-11-11/Animation/AnimProj/Animation/Assets/Scripts/AttackControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackControl : MonoBehaviour {
+    Animator anim = null;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        Debug.Assert(anim);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+        {
+            anim.SetLayerWeight(1, 1.0f);
+        }
+        else
+        {
+            anim.SetLayerWeight(1, 0.0f);
+        }
+	}
+}
