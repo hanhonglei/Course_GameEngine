c8b02e2f3c7b92e0d5bb0884cb96b1bfab1cfe2d
hanhonglei <hanhonglei@cuc.edu.cn>
2018-03-21 11:36:38 +0800

    no message

---
+++ Slides/_NewPractice2015-11-11/Animation/AnimProj/Animation/Assets/TestAnim2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnim2 : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("attack");
+        }
+	}
+}
