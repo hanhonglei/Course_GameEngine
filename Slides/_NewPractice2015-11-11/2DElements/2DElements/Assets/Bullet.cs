4365700424247b7252422c5f1c4dfc8d6eb0882f
hanhonglei <hanhonglei@cuc.edu.cn>
2018-04-13 17:52:10 +0800

    no message

---
+++ Slides/_NewPractice2015-11-11/2DElements/2DElements/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour {
+    public GameObject effect;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            return;
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        Instantiate(effect, pos, rot);
+        Destroy(gameObject);
+    }
+}
---
+++ Slides/_NewPractice2015-11-11/2DElements/2DElements/Assets/Fire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    public Rigidbody bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void FireBullet(Vector3 target)
+    {
+        //Vector3 dir = target - transform.position;
+        //Vector3 xyProject = Vector3.ProjectOnPlane(dir, Vector3.up);
+
+        //Vector3 bulletStartPos = transform.position;
+        //bulletStartPos.y = 1;
+        Rigidbody g = (Rigidbody)GameObject.Instantiate(bullet, transform.position
+            , Quaternion.FromToRotation(Vector3.forward, target));
+        g.velocity = target.normalized * 100;        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("down");
+            //Vector3 desPos;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            FireBullet(ray.direction);
+            //float enter = 0.0f;
+            //Plane firePlane = new Plane(Vector3.up, transform.position);
+            //if (firePlane.Raycast(ray, out enter))
+            //{
+            //    desPos = ray.GetPoint(enter);
+            //    FireBullet(desPos);
+            //}
+        }
+		
+	}
+}
